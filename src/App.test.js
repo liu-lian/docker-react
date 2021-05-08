5e932b44addcab11701a1a7e84458799a1b40909
noname <aa@bb.cc>
2021-05-07 18:07:54 -0700

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I am changed/i);
   expect(linkElement).toBeInTheDocument();
 });
 
